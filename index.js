ab5bb2513f9eac5a6fcc0eb7015b83e3d2afe516
raj073 <rajoni.das@outlook.com>
2022-12-30 16:31:07 +0600

    task update status

--- index.js
+++ index.js
@@ -35,22 +35,94 @@ async function run() {
       const query = {};
       const mytask = await addTaskCollection.find(query).toArray();
       res.send(mytask);
+    });
 
-      //Task Delete API
-      app.delete("/task/:id", async (req, res) => {
-        const id = req.params.id;
-        const filter = { _id: ObjectId(id) };
-        const result = await addTaskCollection.deleteOne(filter);
-        res.send(result);
-      });
+    //Task Delete API
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await addTaskCollection.deleteOne(filter);
+      res.send(result);
+    });
 
-      //Get Id Wise Task API
-      app.get("/mytask/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const task = await addTaskCollection.find(query).toArray();
-        res.send(task);
-      });
+    //Get Id Wise Task API
+    app.get("/mytask/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const task = await addTaskCollection.find(query).toArray();
+      res.send(task);
+    });
+
+    //Get Id Wise Task Details for Private API
+    app.get("/taskdetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const task = await addTaskCollection.find(query).toArray();
+      res.send(task);
+    });
+
+    //update A Task from MyTask
+    app.patch("/mytask/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedTask = req.body;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          taskName: updatedTask.taskName,
+          taskDetails: updatedTask.taskDetails,
+          taskPriority: updatedTask.taskPriority,
+          image: updatedTask.image,
+          assingedTime: updatedTask.assingedTime,
+        },
+      };
+      const result = await addTaskCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    //Get Complete task from Database
+    app.get("/complete-task", async (req, res) => {
+      const id = req.params.id;
+      const filter = { taskStatus: "Completed" };
+      const completeTask = await addTaskCollection.find(filter).toArray();
+      res.send(completeTask);
+    });
+
+    //Make taskStatus Complete to Not Complete
+    app.put("/complete-task/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const notCompleteTask = {
+        $set: {
+          taskStatus: "Not Completed",
+        },
+      };
+      const result = await addTaskCollection.updateOne(
+        filter,
+        notCompleteTask,
+        options
+      );
+      res.send(result);
+    });
+
+    //Update Mytask Component Completed button Completed Status to Not Completed Status
+    app.put("/mytask/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: ObjectId(id) };
+      console.log(filter);
+      const options = { upsert: true };
+      const updatedCompletedStatus = {
+        $set: {
+          taskStatus: "Completed",
+        },
+      };
+      const result = await addTaskCollection.updateOne(
+        filter,
+        updatedCompletedStatus,
+        options
+      );
+      res.send(result);
     });
   } finally {
   }
